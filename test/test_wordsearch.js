137cc554769e5a13c3470fb3c6e586adf3c738ad
Thomas Weideman <thomas.weideman@gmail.com>
2021-02-11 00:37:35 +0000

    Implementing word search and tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,52 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'J', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'V', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'A', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JAVA')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'J', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'G', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'D', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'T', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'O', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'H', 'Q', 'U', 'A', 'L'],
+    ], 'HOTDOG')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present backwards horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'J', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'G', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'D', 'A', 'V', 'A', 'J'],
+      ['U', 'B', 'T', 'T', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'O', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'H', 'Q', 'U', 'A', 'L'],
+    ], 'JAVA')
+
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pairing with Syd Sisco
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let transposed = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    transposed.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      transposed[i].push(matrix[j][i]);
+    }
+  }
+  return transposed;
+};
+
+const search = (letters, word) => {
+  const reverseWord = word.split("").reverse().join("");
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word) || l.includes(reverseWord)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const wordSearch = (letters, word) => {
+  let found;
+
+  // search horizontally
+  found = search(letters, word);
+  if (found) return true;
+
+  // search vertically by transposing the array
+  const trans = transpose(letters);
+  found = search(trans, word);
+  if (found) return true;
+
+  return false;
+};
+
+module.exports = wordSearch;
